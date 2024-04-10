996ced27144ecebd2aa328a6947172565a27c18d
zhenhongswell <zhenhongswell@gmail.com>
2024-04-10 14:48:36 +0800

    finish the Grade classification

---
+++ If else statement/Homework:Grade classification/index.js
@@ -0,0 +1,17 @@
+const score = 60;
+
+if (score >= 90) {
+  console.log("very good");
+}
+if (score <= 89 && score >= 80) {
+  console.log("good");
+}
+if (score <= 79 && score >= 70) {
+  console.log("medium");
+}
+if (score <= 69 && score >= 60) {
+  console.log("OK");
+}
+if (score < 60) {
+  console.log("not OK!");
+}
