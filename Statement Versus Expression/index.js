b63c1af1d9126ddb8a90cb2b3702623970dbfc6b
zhenhongswell <zhenhongswell@gmail.com>
2024-04-10 13:56:08 +0800

    Expression: a value Statement: what to do

---
+++ Statement Versus Expression/index.js
@@ -0,0 +1,10 @@
+// this is a statement
+let x = 0;
+// this is a expression
+x = 1;
+// this is a expression but also a statement
+// x + 1 is a statement
+// x = x + 1 is a expression
+x = x + 1;
+
+console.log(1);
