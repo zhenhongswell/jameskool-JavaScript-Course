c7447b34a3ae7b23b5fc1e6b8e447cccbb8b708a
zhenhongswell <zhenhongswell@gmail.com>
2024-04-10 14:18:20 +0800

    review the if else statement

---
+++ If else statement/statement/index.js
@@ -0,0 +1,10 @@
+const a = 3;
+const b = 2;
+
+if (a > b) {
+  console.log(`a:${a} is bigger than b:${b}`);
+} else if (a === b) {
+  console.log(`a:${a} is equals to b:${b}`);
+} else {
+  console.log(`a:${a} is lesser than b:${b}`);
+}
